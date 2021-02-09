3697c89a2ef106eac6f657217b02791a3967500e
Guilherme <ggn.noronha@gmail.com>
2021-02-08 22:20:51 -0300

    Edit Search page and Favorites with Magnus

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import {  Text, Div } from 'react-native-magnus';
-
+import { Text, Div } from 'react-native-magnus';
 
 export default function Favorites() {
-    return (
-        <Div p={30}  style={styles.container} rounded="md" shadow="sm">
-            <Text  color="purple500">Favorites</Text>
-        </Div>
-    );
+  return (
+    <Div p={30} style={styles.container} rounded="md" shadow="sm">
+      <Text color="purple500">Favorites</Text>
+    </Div>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,30 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { StyleSheet } from 'react-native';
-import {  Text, Div, Input, Icon} from 'react-native-magnus';
-
-
+import { Text, Div, Input, Icon } from 'react-native-magnus';
+import { BooksContext } from '../../context/BookContext';
 
 export default function Search() {
+  const { paramSearch, setParamSearch } = useContext(BooksContext);
 
-    return (
-        <Div p={30}  style={styles.container} rounded="md" shadow="sm">
-            <Input
-                placeholder="Username"
-                p={10}
-                focusBorderColor="blue700"
-                returnKeyType='search'
-                suffix={<Icon name="search" color="gray900" fontFamily="Feather" />}
-                clearButtonMode="while-editing"
-            />
-        </Div>
-    );
+  return (
+    <Div p={30} style={styles.container} rounded="md" shadow="sm">
+      <Input
+        placeholder="Username"
+        p={10}
+        focusBorderColor="blue700"
+        returnKeyType="search"
+        suffix={<Icon name="search" color="gray900" fontFamily="Feather" />}
+        onSubmitEditing={() => {
+          console.log(paramSearch);
+        }}
+        onChange={(event) => {
+          setParamSearch(event.nativeEvent.text);
+        }}
+        value={paramSearch}
+        clearButtonMode="while-editing"
+      />
+    </Div>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
