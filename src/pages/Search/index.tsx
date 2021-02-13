0684436c5eed43a5a82111276b93fdd4f696d3b6
Guilherme <ggn.noronha@gmail.com>
2021-02-13 17:49:29 -0300

    Change layout SearchPage - InputText on top, new colors and spacings

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,22 +1,77 @@
-import React, { useContext, useMemo, useRef, useState } from 'react';
-import { Div, Input, Icon, Button } from 'react-native-magnus';
-import { SafeAreaView, Keyboard } from 'react-native';
+import React, { useContext, useMemo, useRef } from 'react';
+import { Div, Input, Icon, Button, Text } from 'react-native-magnus';
+import { Keyboard } from 'react-native';
 import LottieView from 'lottie-react-native';
 import { BooksContext } from '../../context/BookContext';
 import ListBooks from '../../components/ListBooks';
 import COLORS from '../../styles/colors';
+import Metrics from '../../styles/metrics';
 
 export default function Search() {
   const { searchBooks, booksSearchList } = useContext(BooksContext);
   const refInput = useRef();
 
   const handleSearch = (keepList = false) => {
+    console.log(refInput.current.inputTextValue);
     searchBooks(refInput.current.inputTextValue, keepList);
     Keyboard.dismiss();
   };
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Div flex={1} alignItems="center" bg={COLORS.bgPageSearch}>
+    <Div bg={COLORS.bgPage}>
+      <Div mb={20} alignSelf="flex-start" mx={Metrics.mediumSpacing} mt={20}>
+        <Text
+          fontWeight="bold"
+          fontSize={28}
+          textAlign="left"
+          color="white"
+          lineHeight={32}
+          mb={4}
+        >
+          Busca
+        </Text>
+        <Div
+          style={{
+            borderBottomColor: COLORS.white,
+            borderBottomWidth: 1,
+            width: 40,
+          }}
+        />
+
+        <Input
+          mt={40}
+          bg={COLORS.bgPage}
+          style={{ width: '100%' }}
+          placeholder="Pesquise pelo livro aqui"
+          borderColor={COLORS.white}
+          fontSize={16}
+          color="white"
+          focusBorderColor={COLORS.white}
+          returnKeyType="search"
+          suffix={
+            <Button
+              bg="transparent"
+              h={40}
+              w={40}
+              rounded="circle"
+              onPress={() => {
+                handleSearch();
+              }}
+            >
+              <Icon name="search" color="white" fontFamily="Feather" />
+            </Button>
+          }
+          ref={refInput}
+          onSubmitEditing={() => {
+            handleSearch();
+          }}
+          onChangeText={(text) => {
+            refInput.current.inputTextValue = text;
+          }}
+          clearButtonMode="while-editing"
+        />
+      </Div>
+      <Div>
         <ListBooks
           booksSearchList={booksSearchList.items}
           onEndReached={() => handleSearch(true)}
@@ -35,43 +90,9 @@ export default function Search() {
               []
             )
           }
-        />
-        <Input
-          bg={COLORS.primary}
-          style={{ position: 'absolute', bottom: 0 }}
-          mb={10}
-          mr={10}
-          ml={10}
-          placeholder="Pesquise pelo livro aqui"
-          borderColor="transparent"
-          p={10}
-          fontSize={16}
-          color="white"
-          focusBorderColor={COLORS.primary}
-          returnKeyType="search"
-          suffix={
-            <Button
-              bg="white"
-              h={40}
-              w={40}
-              rounded="circle"
-              onPress={() => {
-                handleSearch();
-              }}
-            >
-              <Icon name="search" color="black" fontFamily="Feather" />
-            </Button>
-          }
-          ref={refInput}
-          onSubmitEditing={() => {
-            handleSearch();
-          }}
-          onChangeText={(text) => {
-            refInput.current.inputTextValue = text;
-          }}
-          clearButtonMode="while-editing"
+          paddingBottom={450}
         />
       </Div>
-    </SafeAreaView>
+    </Div>
   );
 }
